c1ca0850252bb01bdba7101698e9bd26db749cbc
Luis Pellizzon <92530249+luispellizzon@users.noreply.github.com>
2026-07-30 16:58:51 +0100

    Add: reorganize-string - submission-3

---
+++ Data Structures & Algorithms/reorganize-string/submission-3.java
@@ -0,0 +1,53 @@
+
+
+class Solution {
+    public class CharCount{
+        char ch;
+        int count;
+        public CharCount(char ch, int count){
+            this.ch = ch;
+            this.count = count;
+        };
+    }
+    public String reorganizeString(String s) {
+        Map<Character, Integer> charFrequency = new HashMap<>();
+
+        for(char ch: s.toCharArray()){
+            charFrequency.putIfAbsent(ch, 0);
+            charFrequency.put(ch, charFrequency.get(ch) + 1);
+        }
+
+        PriorityQueue<CharCount> maxHeap = new PriorityQueue<>((a,b) -> {
+            return Integer.compare(b.count, a.count);
+        });
+
+        for(Map.Entry<Character, Integer> charEntry: charFrequency.entrySet()){
+            char ch = charEntry.getKey();
+            int count = charEntry.getValue();
+            maxHeap.add(new CharCount(ch, count));
+        }
+
+        CharCount prev = null;
+        StringBuilder res = new StringBuilder();
+        while(!maxHeap.isEmpty()){
+            CharCount current = maxHeap.poll();
+            res.append(current.ch);
+            current.count -= 1;
+            
+            if(prev != null){
+                maxHeap.add(prev);
+                prev = null;
+            }
+
+            if(current.count > 0){
+                prev = current;
+            }
+        }
+        
+        if(prev != null){
+            return "";
+        }
+
+        return res.toString();
+    }
+}
